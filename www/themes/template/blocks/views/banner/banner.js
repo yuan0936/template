5a8ef4dad31211759db1194ce7a24751cbd32655
yuan0936 <yuan@gocharm.com.tw>
2014-12-03 14:55:48 +0800

    modified banner timer

--- www/themes/template/blocks/views/banner/banner.js
+++ www/themes/template/blocks/views/banner/banner.js
@@ -6,33 +6,43 @@ ctrl.startup = function() {
 };
 
 function nav() {
-	var i=0;
+	var serial=0;
 	ctrl.sel('#sequence ul li').hide();
 	ctrl.sel('#sequence ul li:eq(0)').show();	
 
-    // mobile、screen(max-width:820px)
+    // mobile & screen(max-width:820px)
     ctrl.sel('.prev').click(function() {
-    	i--;
-	    if(i<0){i=0}
+    	serial--;
+	    if(serial<0){serial=0};
         ctrl.sel("#sequence ul li").hide();
-        ctrl.sel("#sequence ul li:eq("+i+")").fadeIn(2000);        
+        ctrl.sel("#sequence ul li:eq("+serial+")").fadeIn(1000);        
     });
-
     ctrl.sel('.next').click(function() {
-    	i++;
-	    if(i>2){i=2}
+    	serial++;
+	    if(serial>2){serial=2};
         ctrl.sel("#sequence ul li").hide();
-        ctrl.sel("#sequence ul li:eq("+i+")").fadeIn(2000);        
-    });
+        ctrl.sel("#sequence ul li:eq("+serial+")").fadeIn(1000);        
+    });       
 
-    // desktop、tablet
+    // desktop & tablet
     ctrl.sel('#nav li').click(function(event) {
-        var num=$(this).index()
-
+        var number=$(this).index();        
         ctrl.sel("#sequence ul li").hide();
-        ctrl.sel("#sequence ul li:eq("+num+")").fadeIn(2000);
+        ctrl.sel("#sequence ul li:eq("+number+")").fadeIn(1000);
 
-        ctrl.sel("#nav li span").removeClass("active")
-        ctrl.sel("#nav li span:eq("+num+")").addClass("active")             
+        ctrl.sel("#nav li span").removeClass("active");
+        ctrl.sel("#nav li span:eq("+number+")").addClass("active");             
     }); 
+
+    // Timer
+    ctrl.sel("#sequence").everyTime(7000, timer);
+    function timer(){
+        serial++;
+        if(serial>2){serial=0};       
+        ctrl.sel("#sequence ul li").hide();
+        ctrl.sel("#sequence ul li:eq("+serial+")").fadeIn(3000);
+        ctrl.sel("#nav li span").removeClass("active");
+        ctrl.sel("#nav li span:eq("+serial+")").addClass("active");
+    }; 
 };
+
